938217a720a7c0f68aca5302429c472f0ca482c3
Austin-Senna <austinsenna@gmail.com>
2026-02-10 23:36:33 -0600

    tts setup

--- loader.py
+++ loader.py
@@ -18,3 +18,18 @@ def audio_generator(file_list, processor = None):
     for file_name in file_list:
         audio, _ = librosa.load(file_name, sr=target_sr)
         yield audio, file_name  
+
+def find_json(path):
+    path = os.path.join(os.getcwd(), path)
+    list_files = glob.glob(f'{path}/*.json?')
+    list_files.sort()
+
+    return list_files
+
+def load_json(path):
+    import pandas as pd    
+    name = os.path.basename(path)
+    df = pd.read_json(path_or_buf=path, lines=True)
+    
+    sentences = df[['sentence_good', 'sentence_bad']].values.tolist()
+    return [{"id": f"{name}_{i}", "good": sentence[0], "bad": sentence[1]} for i, sentence in enumerate(sentences)]
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from loader import load, audio_generator
+from loader import load, audio_generator, load_json, find_json
 import os
-audios = load('mald1/MALD1_pw/')
-subset_audios = audios[:10]
-print(subset_audios)
-print("loaded audio")
+
+
+files = find_json('blimp')
+file = files[0]
+print(load_json(file)[:10])
+# print(files)
+# audios = load('mald1/MALD1_pw/')
+# subset_audios = audios[:10]
+# print(subset_audios)
+# print("loaded audio")
 
 # os.makedirs(f"{os.getcwd()}/output_features", exist_ok=True)
 # for i, (audio, file_name) in enumerate(audio_generator(subset_audios, processor=None)):
